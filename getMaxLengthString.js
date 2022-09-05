08458c5c9abe349655464d464e21b42f5ceb9a19
MILLOR <89179883+THANAWINs@users.noreply.github.com>
2022-09-06 02:18:39 +0700

    test1

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -2,5 +2,17 @@ const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
   //code here
+  let b = []
+  let max = 0
+  if(arrayOfString === null || arrayOfString === undefined) return undefined
+  for(x of arrayOfString){
+    max = x.length > max?x.length:max
+  }
+  for(x of arrayOfString){
+    if(x.length == max){
+        b.push(x)
+    }
+  }
+  return b
 }
 module.exports = getMaxLengthString
